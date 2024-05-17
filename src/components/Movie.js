c99ff192f00fcf02128dcbcedc0d20d292146164
elya <vavina.elvira@gmail.com>
2024-05-17 04:01:51 +0300

    img before title and img-placeholder

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,8 +3,8 @@ import React from 'react';
 function Movie(props) {
     const {Title, Type, Year, Poster} = props.info
     return <div className="movie card">
-    <span className="card-title">{Title}</span>
-        <img src={Poster} className='card-img'/>
+        <img src={Poster !== 'N/A' ? Poster : "https://placehold.co/100x140?text="+Title} className='card-img'/>
+        <span className="card-title">{Title}</span>
         <p>{Type}
         <br/>
         {Year}</p>
